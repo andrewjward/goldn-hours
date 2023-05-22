8741db0143be19287b845b39e3683704483dcf22
karbassioon-d <112239943+karbassioon-d@users.noreply.github.com>
2023-05-22 11:37:52 -0700

    made routes and started the nav

--- ghi/src/App.js
+++ ghi/src/App.js
@@ -3,6 +3,7 @@ import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api";
 import "./App.css";
 import Nav from "./components/Nav";
 import SignupForm from "./components/SignupForm";
+import { BrowserRouter, Route } from "react-router-dom";
 
 function App() {
   const { isLoaded } = useLoadScript({
@@ -10,7 +11,29 @@ function App() {
   });
 
   if (!isLoaded) return <div>Loading...</div>;
-  return <Map />;
+  return (
+    <BrowserRouter>
+      <Nav />
+      <div>
+        <Route path="/" element={<Main />} />
+
+        <Route path="/signup" element={<SignupForm />} />
+
+        <Route path="Login" >
+          <Route path="" element={<Login />} />
+          <Route path="/logout" element={<Logout />} />
+        </Route>
+
+        <Route path="/pins" >
+          <Route path="/create" element={<CreatePin />} />
+          <Route path="/pins/location/:id" element={<Location />} />
+          <Route path="pins/profile/:id" element={<Profile />} />
+        </Route>
+      </div>
+      {/* <Map />
+      <SignupForm /> */}
+    </BrowserRouter>
+  );
 }
 
 function Map() {
@@ -18,12 +41,11 @@ function Map() {
 
   return (
     <div className="">
-      <Nav />
       <div
         className="rounded-2xl flex items-center justify-center"
         style={{ width: "100vw", height: "100vh" }}
       >
-        {/* <div className="rounded-2xl h-4/5 w-4/5 flex items-center justify-center">
+        <div className="rounded-2xl h-4/5 w-4/5 flex items-center justify-center">
           <GoogleMap
             zoom={5.3}
             center={center}
@@ -31,8 +53,7 @@ function Map() {
           >
             <Marker position={center} />
           </GoogleMap>
-        </div> */}
-        <SignupForm />
+        </div>
       </div>
     </div>
   );
--- ghi/src/components/Nav.js
+++ ghi/src/components/Nav.js
@@ -1,15 +1,49 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 const Nav = () => {
      return (
-    <nav className="bg-black">
-        <ul className="navbar-nav me-auto ml-auto d-flex flex-wrap mb-2 mb-lg-0">
-            <li className="nav-item dropdown">
-                <div className="nav-link active" aria-current="page" to="/manufacturers">Manufacturers</div>
-            </li>
-        </ul>
-    </nav>
-  )
+       <nav className="bg-lime-400">
+         <div className="flex justify-between">
+           <ul className="flex justify-center items-center">
+             <li className="">
+               <NavLink className="" aria-current="page" to="/main">
+                 Gold'n Hours
+               </NavLink>
+             </li>
+             <li className="">
+               <NavLink className="" aria-current="page" to="/">
+                 My Profile
+               </NavLink>
+             </li>
+             <li className="">
+               <NavLink className="" aria-current="page" to="/">
+                 Search Users
+               </NavLink>
+             </li>
+             <li className="">
+               <NavLink className="" aria-current="page" to="/">
+                 New Post +
+               </NavLink>
+             </li>
+           </ul>
+           <div className='flex'>
+             <input
+               type="search"
+               id="default-search"
+               className="p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+               placeholder="Search Locations"
+             ></input>
+             <button
+               type="submit"
+               className="text-white right-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+             >
+               Search
+             </button>
+           </div>
+         </div>
+       </nav>
+     );
 }
 
 export default Nav
