d13aa58cc3aa49efe3a906f048606e2c1a265ff8
Craig Johnson <craigjart@gmail.com>
2023-06-02 12:22:05 -0400

    Fixed my weird nav issue

--- ghi/src/components/Nav.js
+++ ghi/src/components/Nav.js
@@ -4,7 +4,6 @@ import logo from "../images/gold-icon.png";
 import useToken from "@galvanize-inc/jwtdown-for-react";
 import { useAuthContext } from "@galvanize-inc/jwtdown-for-react";
 import { useNavigate } from "react-router-dom";
-import SearchBox from "./SearchBox";
 
 const Nav = ({ username }) => {
   const { logout } = useToken();
@@ -15,12 +14,7 @@ const Nav = ({ username }) => {
     logout();
     // setUserData({});   ! doeES THIS DO ANYTHING???
     navigate("/");
-<<<<<<< HEAD
-  }
-  console.log(userData);
-=======
   };
->>>>>>> 8ac96c1ebc9ba8dff2810fd62fcfe1378c84061b
   return (
     <nav className="bg-slate-700 text-white">
       <div className="flex justify-between">
@@ -36,19 +30,11 @@ const Nav = ({ username }) => {
             </NavLink>
           </li>
           <li className="">
-<<<<<<< HEAD
-<<<<<<< HEAD
-            <NavLink className="m-2" aria-current="page" to={`/profile/${userData.username}`}>
-=======
-            <NavLink className="m-2" aria-current="page" to={`/profile/${username}`}>
->>>>>>> 092e93fecae564527a5d5879a9b103ff4d945cc3
-=======
             <NavLink
               className="m-2"
               aria-current="page"
               to={`/profile/${username}`}
             >
->>>>>>> 8ac96c1ebc9ba8dff2810fd62fcfe1378c84061b
               My Profile
             </NavLink>
           </li>
@@ -64,7 +50,18 @@ const Nav = ({ username }) => {
           </li>
         </ul>
         <div className="flex">
-          {/* <SearchBox /> */}
+          <input
+            type="search"
+            id="default-search"
+            className="m-2 p-3 pl-10 text-sm text-orange-900 border border-orange-300 rounded-lg bg-orange-50 focus:ring-orange-500 focus:border-orange-500 dark:bg-orange-700 dark:border-orange-600 dark:placeholder-orange-400 dark:text-white dark:focus:ring-orange-500 dark:focus:border-orange-500"
+            placeholder="Search Locations"
+          ></input>
+          <button
+            type="submit"
+            className="m-2 text-white right-2.5 bg-amber-600 hover:bg-orange-400 focus:ring-4 focus:outline-none focus:ring-orange-400 font-medium rounded-lg text-sm px-4 py-2 dark:bg-orange-600 dark:hover:bg-orange-700 dark:focus:ring-orange-800"
+          >
+            Search
+          </button>
           {token ? (
             <button
               onClick={handleLogout}
