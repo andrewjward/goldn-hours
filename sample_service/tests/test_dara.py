6472c7169524e85b23ae1a91d5ccea2aedf97925
karbassioon-d <112239943+karbassioon-d@users.noreply.github.com>
2023-06-05 13:42:33 -0700

    made the view more button on PinCards functional

--- sample_service/tests/test_dara.py
+++ sample_service/tests/test_dara.py
@@ -1,5 +1,5 @@
 from fastapi.testclient import TestClient
-from auth import authenticator
+from sample_service.authenticator import authenticator
 from main import app
 from queries.pins import PinsQueries
 
@@ -10,11 +10,11 @@ class CreatePin:
         result = {
             "username": "bob",
             "location_name": "New York, NY",
-            "longitude": 69.69,
-            "latitude": 69.69,
+            "longitude": 49.65,
+            "latitude": 12.88,
             "cloudy": 6,
             "windy": 9,
-            "crowded": 6,
+            "crowded": 2,
             "date": "6-22-2023",
             "image_url": "https://cdn.vox-cdn.com/thumbor/EtesRJ6lRBSoivaHZMdv9L8C8ro=/0x0:2000x1330/1200x800/filters:focal(840x505:1160x825)/cdn.vox-cdn.com/uploads/chorus_image/image/66852958/MGMGrand_hearts.0.jpg",
             "id": "1",
@@ -25,13 +25,14 @@ class CreatePin:
 mock_data = {
     "username": "bob",
     "location_name": "New York, NY",
-    "longitude": 69.69,
-    "latitude": 69.69,
+    "longitude": 49.65,
+    "latitude": 12.88,
     "cloudy": 6,
     "windy": 9,
-    "crowded": 6,
+    "crowded": 2,
     "date": "6-22-2023",
     "image_url": "https://cdn.vox-cdn.com/thumbor/EtesRJ6lRBSoivaHZMdv9L8C8ro=/0x0:2000x1330/1200x800/filters:focal(840x505:1160x825)/cdn.vox-cdn.com/uploads/chorus_image/image/66852958/MGMGrand_hearts.0.jpg",
+
 }
 
 
@@ -47,11 +48,11 @@ def test_create_pin():
     json = {
         "username": "bob",
         "location_name": "New York, NY",
-        "longitude": 69.69,
-        "latitude": 69.69,
+        "longitude": 49.65,
+        "latitude": 12.88,
         "cloudy": 6,
         "windy": 9,
-        "crowded": 6,
+        "crowded": 2,
         "date": "6-22-2023",
         "image_url": "https://cdn.vox-cdn.com/thumbor/EtesRJ6lRBSoivaHZMdv9L8C8ro=/0x0:2000x1330/1200x800/filters:focal(840x505:1160x825)/cdn.vox-cdn.com/uploads/chorus_image/image/66852958/MGMGrand_hearts.0.jpg",
         "id": "1",
