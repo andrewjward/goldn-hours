76482f5df468923ef18f2c849b75ef4379600b4c
karbassioon-d <112239943+karbassioon-d@users.noreply.github.com>
2023-06-06 08:17:40 -0700

    nav bar logo width change, added location list username and link

--- ghi/src/components/SearchUser.js
+++ ghi/src/components/SearchUser.js
@@ -48,7 +48,7 @@ function SearchUserList() {
                 className="m-2 text-white right-2.5 bg-amber-600 hover:bg-orange-400 focus:ring-4 focus:outline-none focus:ring-orange-400 font-medium rounded-lg text-sm px-4 py-2 dark:bg-orange-600 dark:hover:bg-orange-700 dark:focus:ring-orange-800"
                 onClick={handleSearchSubmit}
                 whileHover={{
-                  scale: 1.1,
+                  scale: 1.05,
                   transition: { duration: 0.1 },
                 }}
                 whileTap={{ scale: 0.9 }}
